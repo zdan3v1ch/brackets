342d72f6b6c42170e822effeb68ccfa1aad760af
zdanevich <anatoli.zdanevitch@gmail.com>
2022-08-01 11:10:45 +0300

    feat: it works!!!

--- src/index.js
+++ src/index.js
@@ -15,55 +15,74 @@ function check(str, bracketsConfig) {
 
   let openBrack = [];
   let brackPair = {}
-  for (i=0; i < bracketsConfig.length; i++){
+  for (i = 0; i < bracketsConfig.length; i++) {
     openBrack.push(bracketsConfig[i][0])
     brackPair[bracketsConfig[i][1]] = bracketsConfig[i][0]
 
   }
-console.log(openBrack)
-console.log(brackPair)
-// console.log(str)
+  // console.log(openBrack)
+  // console.log(brackPair)
+  // console.log(str)
 
 
   let stack = [];
 
 
-  for (i=0; i < str.length; i++) {
+  for (i = 0; i < str.length; i++) {
     let curSym = str[i];
 
-    console.log(curSym)
+    // console.log(curSym, "curSym")
     if (openBrack.includes(curSym)) {
-console.log(brackPair[curSym])
-      if (brackPair[curSym] === stack[stack.length -1]){
+
+      if (brackPair[stack[stack.length - 1]] === curSym) {
         stack.pop()
       } else {
-      stack.push(curSym)
-      console.log(stack)}
+        stack.push(curSym)
+        // console.log(stack, "this stack")
+      }
     } else {
       if (stack.length === 0) {
-        console.log("false mid")
+        // console.log("false mid")
         return false;
       }
 
 
-      let topElem = stack[stack.length -1];
+      let topElem = stack[stack.length - 1];
 
-      if(brackPair[curSym] === topElem) {
+      if (brackPair[curSym] === topElem) {
         stack.pop();
-      } else {
-        console.log("false dno")
-        return false;
       }
 
 
     }
   }
-  if (stack.length === 0) {
-    console.log("true - last")
-  } else {
-    console.log("false - last")
-  }
- return stack.length === 0;
+  // if (stack.length === 0) {
+  //   console.log("true - last")
+  // } else {
+  //   console.log("false - last")
+  // }
+  return stack.length === 0;
 }
 
-check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])(())', config7)
+
+
+// check('()', config1) // true
+// check('((()))()', config1)// true
+// check('())(', config1)// false
+// check('([{}])', config3)// true
+// check('[(])', config2)// false
+// check('[]()', config2)// true
+// check('[]()(', config2)// false
+// check('||', config4)// true
+// check('|()|', config5)// true
+// check('|(|)', config5)// false
+// check('|()|(||)||', config5)// true !!!!!!!
+// check('111115611111111222288888822225577877778775555666677777777776622222', config6)// true
+// check('5555512575557777777555566667888888667661133833448441111222233333444442266666', config6)// false
+// check('8888877878887777777888888887777777887887788788887887777777788888888887788888', config6)// false
+// check('111115611111111156111111112222888888222255778777787755556666777777777766222221111222288888822225577877778775555666677777777776622222', config6)// true
+// check('[]][[]', config3)// false
+// check('[]][[]', config2)//false
+// check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]]))()', config7)// false
+// check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])(())', config7) //true
+// check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])((([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])))', config7)// true
